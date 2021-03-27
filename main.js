17bdc94f8d388ff838b0d6d1a32631f40999fc4b
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-03-27 23:33:08 +0900

    210327_downClear(done)

--- main.js
+++ main.js
@@ -65,8 +65,6 @@ function init(){
 
     temp_block = {...virtual_block}; 
     for(let i=0 ; i<rows ; i++){ //테트리스판 행(rows)
-        const li = document.createElement("li");
-        board.prepend(li); 
         addLine();
     }    
 
@@ -74,12 +72,17 @@ function init(){
 }
 
 function addLine(){
-    const rows_ul = document.createElement("ul"); //테트리스 가로 ul
-    board.children[0].prepend(rows_ul);
+    // ul> --> li <-- >ul>li
+    const li = document.createElement("li");
+    // ul > li > --> ul <-- > li
+    const ul = document.createElement("ul");
+     
     for(let j=0 ; j< col ; j++){ //테트리스판 열(column)
         const tetris_block = document.createElement("li"); // 테트리스 기본 블록
-        board.children[0].children[0].prepend(tetris_block); //prepend로 앞에 생성되기 때문에 [0]
+        ul.prepend(tetris_block); //prepend로 앞에 생성되기 때문에 [0]
     }
+    li.prepend(ul);
+    board.prepend(li); 
 }
 
 function Rendering(edgeCase = ''){ // 엣지케이스 구분을 위해 
@@ -88,7 +91,7 @@ function Rendering(edgeCase = ''){ // 엣지케이스 구분을 위해
 
     const movingBlocks = document.querySelectorAll('.moving');
     // 블록 이동효과와 엣지 케이스를 위해 moving 클래스를 모두 선언
-
+    
     movingBlocks.forEach(moveing =>{
         moveing.classList.remove(form, 'moving'); 
         //moving를 제거해 이동된 자리에 클래스만 남아 css로 블록모양이 유지됨
@@ -97,37 +100,30 @@ function Rendering(edgeCase = ''){ // 엣지케이스 구분을 위해
 
     blocks[form][direction].some(block => {
 
-        const x = block[0] + left; //블록 변동 위치 저장
+        const x = block[0] + left; //블록이 이동될 좌표 저장
         const y = block[1] + top; 
-
+        //이동될 좌표에 <li>요소 유무 확인 ↓
         const chk_block = board.children[y] ? board.children[y].children[0].children[x] : null; 
-        
-        if(check_block(chk_block)){
+        // ↓ 다음으로 이동 될 좌표에 <li> 요소의 유무와 블록이 존재하는 경우를 체크
+        const isAvailable = check_block(chk_block);
 
-            //해당 위치에 블럭 모양의 클래스이름 추가 및 이동 효과를 위한 moved 클래스도 추가
+        if(isAvailable){
+            //해당 위치에 블럭 모양의 클래스이름 추가 및 이동 효과를 위한 moving 클래스 추가
             chk_block.classList.add(form, 'moving'); 
             console.log("rending")
-        }
-        else{
-            //엣지케이스..
-            if(edgeCase === 'top'){ //내려오다가 걸린경우 블록을 바닥에 고정
-                temp_block = {...virtual_block};
-
-                setTimeout(()=>{
-                    Rendering();
-                    freezeBlock();
-                }, 0)
-                
-                console.log("바닥 터치!");
-            }
-            else{ // 좌우 영역을 넘어갔을 경우 이전 위치로 재이동
+        }else{ //엣지케이스 (좌우범위 이탈과 바닥 터치했을 경우)
                 temp_block = {...virtual_block};
                 console.log('좌우 범위 이탈')
                 setTimeout(()=>{
                     Rendering();
+
+                    if(edgeCase === 'top'){ //바닥 터치
+                        console.log("바닥 터치!");
+                        freezeBlock();  //블록 고정
+                    }
                 }, 0)
+                return true;
             }
-        }
         console.log('left : ' + x + ' / Top : ' + y);
     });
 
@@ -139,31 +135,60 @@ function Rendering(edgeCase = ''){ // 엣지케이스 구분을 위해
 }
 
 function check_block(target){ 
-    if(target){
-        return true;
-    }else{
+    // ↓ 현재 위치 하단의 <li> 요소의 유무 / 고정된 블록이 존재하는 경우 체크
+    if( !target || target.classList.contains('freeze')){
         return false;
-    }
+    }else{
+        return true;
+    }    
 }
 
 function freezeBlock(){ //바닥에서 블록 고정
+    // ↓ 현재 이동중인 moving 클래스를 선언
     const freezeBlock = document.querySelectorAll('.moving');
     
     freezeBlock.forEach(block =>{
-        block.classList.remove('moving');
-        block.classList.add('freeze');
+        block.classList.replace('moving', 'freeze'); 
+    //선언한 moving 클래스를 freeze로 변경하여 기존 블럭 모양은 css로 유지
     })
-
-    generate_Block(); //새로운 블록 생성
-
+    clear_Line_check();
 }
 
+function clear_Line_check(){ 
+
+    const chk_line = board.childNodes; //유사배열보다 배열사용이 용이하여 childNodes사용
+    let block_cnt = 0;
+
+    chk_line.forEach(Line =>{
+        Line.childNodes[0].childNodes.forEach( block =>{
+            if(block.classList.contains('freeze')){
+               block_cnt++;
+               if(block_cnt === 10){
+                   Line.remove()
+                   addLine()
+               }
+               console.log(block_cnt)
+            }
+            else{
+                block_cnt = 0;
+            }
+        })
+    })    
+    generate_Block();
+}
+
+
 function generate_Block(){
+
+    let random = (Math.random() * 6).toFixed(0);
+    const form = Object.keys(blocks)[random];
+    virtual_block.form = form;
     virtual_block.top = 0;
     virtual_block.left = 3;
     virtual_block.direction = 0;
     temp_block = {...virtual_block}; 
     Rendering();
+
 }
 
 function moveBlock(move_direction, x){ 
@@ -173,12 +198,12 @@ function moveBlock(move_direction, x){
 }
 
 function changeDirection(){// 블록의 방향을 시계방향으로 움직임
-    if(temp_block['direction'] < 3){
-        temp_block['direction'] += 1;
-    }else{
-        temp_block['direction'] = 0;
-    }
-    console.log(temp_block['direction']);
+    
+    if(temp_block.direction < 3)
+        temp_block.direction++;
+    else
+        temp_block.direction = 0;
+
     Rendering();
 }
 
@@ -209,5 +234,3 @@ document.addEventListener('keydown', e=>{
         }
     }
 })
-
-
